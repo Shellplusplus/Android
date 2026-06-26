1c99a061b34c9a7f3b4fca7c8993a642f63f8e1d
ziyimiao5054 <2766641711@qq.com>
2026-06-26 22:36:35 +0800

    添加主界面 XML 布局：RelativeLayout + TextView 显示 shell++

--- app/src/main/java/com/shell/liangyi/MainActivity.kt
+++ app/src/main/java/com/shell/liangyi/MainActivity.kt
@@ -10,6 +10,7 @@ class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
         wearMessageCenter = WearMessageCenter.getInstance(this)
         wearMessageCenter.initialize()
