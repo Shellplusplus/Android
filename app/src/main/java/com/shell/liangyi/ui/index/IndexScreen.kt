c887773b4c8e4652c0824ac9ead5bb7ee3c4cc54
ziyimiao5054 <2766641711@qq.com>
2026-06-27 01:47:49 +0800

    设置图标往左调整：end 16dp→24dp，对齐 Figma x:946

--- app/src/main/java/com/shell/liangyi/ui/index/IndexScreen.kt
+++ app/src/main/java/com/shell/liangyi/ui/index/IndexScreen.kt
@@ -64,7 +64,7 @@ fun IndexScreen(navController: NavHostController) {
                 onClick = { /* TODO: 设置 */ },
                 modifier = Modifier
                     .align(Alignment.TopEnd)
-                    .padding(top = 38.dp, end = 16.dp)
+                    .padding(top = 38.dp, end = 24.dp)
                     .size(24.dp)
             ) {
                 Icon(
